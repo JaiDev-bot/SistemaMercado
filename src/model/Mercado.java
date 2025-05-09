d3727e988b2a87badb530463460c683fadd70063
Jaiane <jaianesoares700@gmail.com>
2025-05-09 10:53:05 -0300

    Feito métodos adicionar e remover nas categorias e itens.

--- src/model/Mercado.java
+++ src/model/Mercado.java
@@ -44,4 +44,23 @@ public class Mercado {
     public void setItens(ArrayList<Item> itens) {
         this.itens = itens;
     }
+
+    public void adicionarCategoria(Categoria categoria) {
+        categorias.add(categoria);
+    }
+
+    public void removerCategoria(int codigo) {
+        System.out.println(categorias.get(codigo)+ "foi removido com sucesso");
+        categorias.remove(codigo);
+    }
+
+    public void adicionarItem(Item item) {
+        itens.add(item);
+    }
+
+    public void removerItem(int codigo) {
+        System.out.println(itens.get(codigo)+ "foi removido com sucesso");
+        itens.remove(codigo);
+    }
 }
+
